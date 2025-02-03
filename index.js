5a10486bc4326dede3f10094e260fa2fd8d83c9e
vovazaietss <v.zts2005@gmail.com>
2025-02-04 01:39:44 +0200

    create post 'auth/login'

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ import express from 'express'
 const app = express();
 
 
+app.use(express.json()) ;
+
+
 app.get('/',(req,res) => {
     res.send('111Hello world');
 });
+
+
+app.post('/auth/login', (req,res) => {
+    console.log(req.body)
+    res.json({
+        success: true, 
+    });
+});
+
+
 
 app.listen(4444,(err) => {
     if (err) {
