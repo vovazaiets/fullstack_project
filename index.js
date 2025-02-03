c3949e5869dc0c018425652e263cd9d07f4230ec
vovazaietss <v.zts2005@gmail.com>
2025-02-04 01:25:28 +0200

    'hello world' on '/'

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import express from 'express'
+
+
+const app = express();
+
+
+app.get('/',(req,res) => {
+    res.send('111Hello world');
+});
+
+app.listen(4444,(err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server ok');
+});
