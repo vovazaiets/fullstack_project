24cfab48cebfc139d9edb70af45b779020ee001e
vovazaietss <v.zts2005@gmail.com>
2025-02-04 11:11:07 +0200

    create user db/bcrypt password

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 import { validationResult } from 'express-validator';
 import { registerValidator } from './validations/auth.js';
+
+import UserModel from './models/User.js';
 
 mongoose.connect('mongodb+srv://vzts2005:T1tw1idZ26trSDK8@cluster0.sgy9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 ).then(()=> console.log('DB ok'))
@@ -14,14 +17,24 @@ const app = express();
 
 app.use(express.json()) ;
 
-app.post('/auth/register', registerValidator,(req,res) => {
+app.post('/auth/register', registerValidator,async(req,res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array());
     }
-    res.json({
-        success: true,
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password,salt)
+    const doc = new UserModel({
+        email: req.body.email ,
+        fullName: req.body.fullName,
+        passwordHash,
+        avatarUrl: req.body.avatarUrl,
     });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 
